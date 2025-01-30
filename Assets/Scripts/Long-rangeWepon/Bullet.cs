95fd46b7edd6a2aa9f7ae1fe99ff7a24f41ac0f1
Anastasija Kizika <anaski@inbox.lv>
2025-01-30 13:12:21 +0200

    1) fixed shooting 2) made SMOOTH player rotation

--- Assets/Scripts/Long-rangeWepon/Bullet.cs
+++ Assets/Scripts/Long-rangeWepon/Bullet.cs
@@ -3,25 +3,28 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // later here can be bullet sprite
-    private float _demage = 1f, _speed = 1f;
-    private Vector3 _target;
+    private float _demage = 1f, _speed = 5f;
+    // private Vector3 _target;     use target AND in Fized update calculate direction to chase te terget
 
-    public void SetBullet(float demage, float speed, Vector3 target)
+    private Vector3 _direction = new Vector3(0, 1, 0);
+
+    public void SetBullet(float demage, float speed, Vector3 _target)
     {
         _demage = demage;
         _speed = speed;
-        _target = target;
     }
 
     private void FixedUpdate()
     {
-        if (transform.position == _target) Destroy(gameObject);
-        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        transform.Translate(_direction * _speed * Time.deltaTime);
+        if (transform.position.x > 40 || transform.position.y > 40 || transform.position.x < -40 || transform.position.y < -40) Destroy(gameObject); // must be overwrite, couse this is tupid idea
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.GetComponent<EnemyFight>()) collision.GetComponent<EnemyFight>().GetDemage(_demage);
-        if (collision.tag != "Player") Destroy(gameObject);
+        if (collision.gameObject.GetComponent<EnemyFight>())
+        {
+            collision.gameObject.GetComponent<EnemyFight>().GetDemage(_demage);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Joystick joystick;
-    private float _moveSpeed = 5f, _angle, _moveX, _moveY; 
+    private float _moveSpeed = 5f, _angle, _moveX, _moveY, _smoothaAngle, _rottionSpeed; 
     private Rigidbody2D rb;
     private Vector3 _nearestEnemy, _direction;
-
+    private Vector2 _moveInput;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();  
@@ -19,19 +19,20 @@ public class PlayerMovement : MonoBehaviour
         _nearestEnemy = EnemyList.NearestEnemy;
         _direction = transform.position - _nearestEnemy;
         _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, _angle));
+        _smoothaAngle = Mathf.SmoothDampAngle(transform.eulerAngles.z, _angle, ref _rottionSpeed, 0.1f);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, _smoothaAngle));
 
         // Get joystick values
         _moveX = joystick.Horizontal;
         _moveY = joystick.Vertical;
 
-        Vector2 moveInput = new Vector2(_moveX, _moveY);
-        if (moveInput.magnitude > 1)
+        _moveInput = new Vector2(_moveX, _moveY);
+        if (_moveInput.magnitude > 1)
         {
-            moveInput.Normalize(); // Ensures diagonal movement isn't faster
+            _moveInput.Normalize(); // Ensures diagonal movement isn't faster
         }
 
         // Apply movement to Rigidbody
-        rb.velocity = moveInput * _moveSpeed;
+        rb.velocity = _moveInput * _moveSpeed;
     }
 }
